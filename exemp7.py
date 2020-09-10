f040b3946c15e6f7a1f45ff33bdc3976822da694
ValeriyKirsanov <70278649+ValeriyKirsanov@users.noreply.github.com>
2020-09-10 21:39:28 +0300

    Update exemp7.py

--- exemp7.py
+++ exemp7.py
@@ -12,3 +12,20 @@ print(insert_full(name_1=str(input("input name")),
                   Sity_live=str(input("input sity")),
                   adress_mail=str(input("input email")),
                   phone_numb=str(input("input phone"))))
+
+
+from sys import argv
+
+def salary(hours, stav, bounty):
+    return int((hours * stav) + pr)
+
+try:
+    file, w_hours, h_stav, b_pr = argv
+except ValueError:
+    print("Введите аргументы через пробел - кол-во отработанных часов, ставку, премию")
+    exit()
+
+print(w_hours, h_stav, b_pr)
+s = salary(int(w_hours), int(h_stav), int(b_pr))
+
+print(f"Зарплата за отработанные часы + премия : {s}")
